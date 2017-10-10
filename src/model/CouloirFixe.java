1b4bbe71b49e21c0cdf04047bd53400b5793980e
Martin Bolot <bolotmartin@gmail.com>
2017-10-10 21:58:53 +0200

    ajout classes couloir

---
+++ src/model/CouloirFixe.java
@@ -0,0 +1,10 @@
+package model;
+
+/**
+ * Created by Martin on 10/10/2017.
+ */
+public class CouloirFixe extends AbstractCouloir {
+    CouloirFixe(boolean northOpened, boolean southOpened, boolean eastOpened, boolean westOpened, int x, int y){
+        super(northOpened, southOpened, eastOpened, westOpened, x, y);
+    }
+}
