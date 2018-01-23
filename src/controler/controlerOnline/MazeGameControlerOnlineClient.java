26c829a8c72d1faadc6833bb25fe9dd68ecaa7b7
Martin Bolot <bolotmartin@gmail.com>
2018-01-23 23:17:47 +0100

    avancee

--- src/controler/controlerOnline/MazeGameControlerOnlineClient.java
+++ src/controler/controlerOnline/MazeGameControlerOnlineClient.java
@@ -1,116 +1,84 @@
 package controler.controlerOnline;
 
 import model.observable.MazeGame;
-import tools.ReceptionSocket;
-
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Observable;
+import java.util.Observer;
 
-public class MazeGameControlerOnlineClient extends MazeGameControlerOnline {
+public class MazeGameControlerOnlineClient extends MazeGameControlerOnline implements Observer {
     public MazeGameControlerOnlineClient(MazeGame mazeGame, String host, int port) {
         super(mazeGame);
 
         try {
             connexion = new Socket(host, port);
-            writer = new PrintWriter(connexion.getOutputStream(), true);
             this.open();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
-        //this.response = sendMessage("GETMAZEGAME");
-        //this.response = sendMessage("TOTO");
+    }
 
+    @Override
+    public void update(Observable o, Object arg) {
+        System.out.println("update client mazegame");
+
+        // envoi vers le client
+        sendMessage("testDepuisClient");
     }
     
     private void open() {
         Thread ServerHandler = new Thread(new Runnable(){
-    		public void run(){
-    	        System.out.println("Traitement de la connexion serveur- côté client");
-        		while(!connexion.isClosed()){
-    	            try {
-    	                writer = new PrintWriter(connexion.getOutputStream());
-    	
-    	                String query = read();
-    	                String debug;
-    	                debug = "\t -> Commande reçue : " + query + "\n";
-    	                System.err.println("\n" + debug);
-    	                
-    	                if(query == null) { return; }
-    	
-    	                ArrayList<Object> objects = new ArrayList<>();
-    	                objects.add("testfromclienttoserver");
-    	                ObjectOutputStream sortie = new ObjectOutputStream(connexion.getOutputStream());
-    	                sortie.writeObject(objects.size());
-    	                for(Object tmp : objects){
-    	                    sortie.writeObject(tmp);
-    	                }
-    	                sortie.flush();
-    	
-    	                if(query.equals("CLOSE")){
-    	                    System.err.println("COMMANDE CLOSE DETECTEE ! ");
-    	                    writer = null;
-    	                    connexion.close();
-    	                    break;
-    	                }
-    	            }catch(SocketException e){
-    	            	e.printStackTrace();
-    	                System.err.println("LA CONNEXION A ETE INTERROMPUE ! ");
-    	                break;
-    	            } catch (IOException e) {
-    	                e.printStackTrace();
-    	            }
-        		}
-        		System.out.println("isClosed");
-    		}
+            public void run(){
+                System.out.println("Traitement de la connexion serveur- côté client");
+                while(!connexion.isClosed()){
+                    // reception depuis le serveur
+                    ArrayList<Object> reception = recieveUpdate();
+                    if(reception.size() > 0 ) { System.out.println(reception); }
+                    System.out.println(reception);
+
+                    // fermer la connexion
+                    /*if(query.equals("CLOSE")){
+                        System.err.println("COMMANDE CLOSE DETECTEE ! ");
+                        writer = null;
+                        connexion.close();
+                        break;
+                    }*/
+                }
+            }
         });
         ServerHandler.start();
     }
     
-    private String read() throws IOException{
-        BufferedInputStream reader = new BufferedInputStream(connexion.getInputStream());;
-        String response = null;
-        int stream = 0;
-    	try{
-            byte[] b = new byte[4096];
-            stream = reader.read(b);
-            if(stream != -1) {
-                response = new String(b, 0, stream);
-            }
-    	} catch(java.lang.StringIndexOutOfBoundsException ex) {
-    		System.out.println(stream);
-    		ex.printStackTrace();
-		}
-        return response;
-    }
-    
-    private ArrayList<Object> sendMessage(String command) {
-    	if(command == null) {
-    		return null;
-    	}
-        writer.write(command);
-        writer.flush();
-        
-        // lecture et retour du message
-        ArrayList<Object> recieveObjects = new ArrayList<>();
-        try {
-            ObjectInputStream entree = new ObjectInputStream(connexion.getInputStream());
-            int taille = (int) entree.readObject();
-            for(int i=0; i<taille; i++){
-                recieveObjects.add(entree.readObject());
-            }
-            System.out.println(recieveObjects);
-        } catch (IOException | ClassNotFoundException ex) {
-            Logger.getLogger(ReceptionSocket.class.getName()).log(Level.SEVERE, null, ex);
+    private void sendMessage(Object command) {
+        if(command == null) {
+            return;
         }
-        
-        return recieveObjects.size() > 0 ? (ArrayList<Object>) recieveObjects.get(0) : null;
+
+        try{
+            ArrayList<Object> objects = new ArrayList<>();
+            objects.add(command);
+            ObjectOutputStream sortie = new ObjectOutputStream(connexion.getOutputStream());
+            sortie.writeObject(objects.size());
+            for(Object tmp : objects){
+                sortie.writeObject(tmp);
+            }
+            sortie.flush();
+        } catch(IOException ex) { ex.printStackTrace(); }
+    }
+
+    private ArrayList<Object> recieveUpdate() {
+        ObjectInputStream entree;
+        ArrayList<Object> reception = new ArrayList<>();
+        try {
+            entree = new ObjectInputStream(connexion.getInputStream());
+            int taille = (int) entree.readObject();
+            for(int i = 0; i < taille; i++){
+                reception.add(entree.readObject());
+            }
+        } catch (IOException | ClassNotFoundException ex) { ex.printStackTrace(); }
+        return reception;
     }
 
     private Socket connexion;
-    private ArrayList<Object> response;
-    private PrintWriter writer;
 }
--- src/controler/controlerOnline/MazeGameControlerOnlineServer.java
+++ src/controler/controlerOnline/MazeGameControlerOnlineServer.java
@@ -1,214 +1,113 @@
 package controler.controlerOnline;
 
-import tools.EnvoiSocket;
-import tools.ReceptionSocket;
 import model.observable.MazeGame;
-
-import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class MazeGameControlerOnlineServer extends MazeGameControlerOnline implements Observer{
-    public MazeGameControlerOnlineServer(MazeGame mazeGame, String host, int port) {
-        super(mazeGame);
-
-        try {
-            serverSocket = new ServerSocket(port, 100, InetAddress.getByName(host));
-            System.out.println("server created");
-            this.open();
-        } catch(Exception ex) {
-            ex.printStackTrace();
-        }
-
-        if(this.mazeGame != null) {
-            this.mazeGame.addObserver((Observer) this);
-        }
-    }
-
-    public void close(){
-        this.isRunning = false;
-    }
-    
-	@Override
-	public void update(Observable o, Object arg) {
-		System.out.println("update mazegame");
-
-    	this.sendMessage("testserver");
-		/*if(client != null) {
-			try{
-		        ArrayList<Object> objects = new ArrayList<>();
-		        objects.add("UPDATED_SERVER");
-		        ObjectOutputStream sortie = new ObjectOutputStream(serverSocket.getOutputStream());
-		        sortie.writeObject(objects.size());
-		        for(Object tmp : objects){
-		            sortie.writeObject(tmp);
-		        }
-		        sortie.flush();
-			} catch(IOException e) { e.printStackTrace(); }
-		}*/
+	public MazeGameControlerOnlineServer(MazeGame mazeGame, String host, int port) {
+		super(mazeGame);
+		try {
+			serverSocket = new ServerSocket(port, 100, InetAddress.getByName(host));
+			System.out.println("server created");
+			this.open();
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		if(this.mazeGame != null) {
+			this.mazeGame.addObserver(this);
+		}
 	}
 
-    private void open(){
-    	/*
-        Thread t = new Thread(new Runnable(){
-            public void run(){
-                while(isRunning){
-                    try {
-                        //On attend une connexion d'un client
-                        client = serverSocket.accept();
+	@Override
+	public void update(Observable o, Object arg) {
+		System.out.println("update server mazegame");
 
-                        //Une fois reçue, on la traite dans un thread séparé
-                        System.out.println("Connexion cliente reçue.");
+		// envoi vers le client
+		sendMessage("testDepuisServ");
+	}
 
-                        Thread t = new Thread(new ClientProcessor(client, mazeGame));
-                        t.start();
+	private void open(){
+		Thread ClientHandler = new Thread(new Runnable(){
+			public void run(){
+				try {
+					client = serverSocket.accept();
+					System.out.println("Traitement de la connexion cliente - côté serveur");
+					while(!serverSocket.isClosed()){
+						// reception depuis le client
+						/*ArrayList<Object> reception = recieveUpdate();
+						if(reception.size() > 0 ) { System.out.println(reception); }*/
 
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                System.out.println("stopped running");
+						// fermer la connexion
+						/*
+						if(query.equals("CLOSE")){
+						System.err.println("COMMANDE CLOSE DETECTEE ! ");
+						writer = null;
+						reader = null;
+						client.close();
+						break;
+						}
+						*/
+					}
+				}catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		ClientHandler.start();
+	}
 
-                try {
-                    serverSocket.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    serverSocket = null;
-                }
-            }
-        });
-
-        t.start();
-        */
-
-	        Thread ClientHandler = new Thread(new Runnable(){
-	    		public void run(){
-	    	        try {
-		    	        client = serverSocket.accept();
-		    	        System.out.println("Traitement de la connexion cliente - côté serveur");
-		    	        PrintWriter writer = null;
-		    	        BufferedInputStream reader = null;
-		        		while(!serverSocket.isClosed()){
-		    	            try {
-		    	                writer = new PrintWriter(client.getOutputStream());
-		    	                reader = new BufferedInputStream(client.getInputStream());
-		    	
-		    	                String query = read();
-		    	                String debug;
-		    	                debug = "\t -> Commande reçue : " + query + "\n";
-		    	                System.err.println("\n" + debug);
-		    	                
-		    	                if(query == null) { return; }
-		    	
-		    	                ArrayList<Object> objects = new ArrayList<>();
-		    	                objects.add(mazeGame);
-		    	                ObjectOutputStream sortie = new ObjectOutputStream(client.getOutputStream());
-		    	                sortie.writeObject(objects.size());
-		    	                for(Object tmp : objects){
-		    	                    sortie.writeObject(tmp);
-		    	                }
-		    	                sortie.flush();
-		    	
-		    	                if(query.equals("CLOSE")){
-		    	                    System.err.println("COMMANDE CLOSE DETECTEE ! ");
-		    	                    writer = null;
-		    	                    reader = null;
-		    	                    client.close();
-		    	                    break;
-		    	                }
-		    	            }catch(SocketException e){
-		    	            	e.printStackTrace();
-		    	                System.err.println("LA CONNEXION A ETE INTERROMPUE ! ");
-		    	                break;
-		    	            } catch (IOException e) {
-		    	                e.printStackTrace();
-		    	            }
-		        		}
-		    		}catch (IOException e) {
-		                e.printStackTrace();
-		            }
-	    		}
-	        });
-	        ClientHandler.start();
-    }
-    
-    private String read() throws IOException{
-        BufferedInputStream reader = new BufferedInputStream(client.getInputStream());;
-        String response = null;
-        int stream = 0;
-    	try{
-            byte[] b = new byte[4096];
-            stream = reader.read(b);
-            if(stream != -1) {
-                response = new String(b, 0, stream);
-            }
-    	} catch(java.lang.StringIndexOutOfBoundsException ex) {
-    		System.out.println(stream);
-    		ex.printStackTrace();
+	private void sendMessage(Object command) {
+		if(command == null || client == null) {
+			return;
 		}
-        return response;
-    }
-    
-    private ArrayList<Object> sendMessage(String command) {
-    	if(command == null || client == null) {
-    		return null;
-    	}
-    	
-    	try{
-            //writer = new PrintWriter(client.getOutputStream());
-            ArrayList<Object> objects = new ArrayList<>();
-            objects.add(mazeGame);
-            ObjectOutputStream sortie = new ObjectOutputStream(client.getOutputStream());
-            sortie.writeObject(objects.size());
-            for(Object tmp : objects){
-                sortie.writeObject(tmp);
-            }
-            sortie.flush();
-    	} catch(Exception e){ e.printStackTrace(); }
-        
-        // lecture et retour du message
-        ArrayList<Object> recieveObjects = new ArrayList<>();
-        try {
-            ObjectInputStream entree = new ObjectInputStream(client.getInputStream());
-            int taille = (int) entree.readObject();
-            for(int i=0; i<taille; i++){
-                recieveObjects.add(entree.readObject());
-            }
-            System.out.println(recieveObjects);
-        } catch (IOException | ClassNotFoundException ex) {
-        	ex.printStackTrace();
-        }
-        
-        return recieveObjects.size() > 0 ? (ArrayList<Object>) recieveObjects.get(0) : null;
-    }
-    
-    private ServerSocket serverSocket;
-    private Socket client;
-    private boolean isRunning = true;
-    private PrintWriter writer;
-    
-    // tests
-    public static void main(String[] args) {
-    	MazeGameControlerOnlineServer mazeGameControlerOnlineServer = new MazeGameControlerOnlineServer(
+
+		try {
+			ArrayList<Object> objects = new ArrayList<>();
+			objects.add(command);
+			ObjectOutputStream sortie = new ObjectOutputStream(client.getOutputStream());
+			sortie.writeObject(objects.size());
+			for(Object object : objects){
+				sortie.writeObject(object);
+			}
+			sortie.flush();
+		} catch(IOException ex) { ex.printStackTrace(); }
+	}
+
+	private ArrayList<Object> recieveUpdate() {
+		ObjectInputStream entree;
+		ArrayList<Object> reception = new ArrayList<>();
+		try {
+			entree = new ObjectInputStream(client.getInputStream());
+			int taille = (int) entree.readObject();
+			for(int i = 0; i < taille; i++){
+				reception.add(entree.readObject());
+			}
+		} catch (IOException | ClassNotFoundException ex) { ex.printStackTrace(); }
+		return reception;
+	}
+
+	private ServerSocket serverSocket;
+	private Socket client;
+
+	// tests
+	public static void main(String[] args) {
+		MazeGameControlerOnlineServer mazeGameControlerOnlineServer = new MazeGameControlerOnlineServer(
 			null,
 			"127.0.0.1",
 			1234
 		);
-    	MazeGameControlerOnlineClient mazeGameControlerOnlineClient = new MazeGameControlerOnlineClient(
+		MazeGameControlerOnlineClient mazeGameControlerOnlineClient = new MazeGameControlerOnlineClient(
 			null,
 			"127.0.0.1",
 			1234
 		);
-    }
+	}
 }
